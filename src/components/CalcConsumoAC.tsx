d630cbea5235b982a1942b9ea45baa9f16106f55
Diego Ragazzi <ragazzi.d@gmail.com>
2023-09-28 23:09:09 -0300

    improve: style for other components

--- src/components/CalcConsumoAC.tsx
+++ src/components/CalcConsumoAC.tsx
@@ -26,68 +26,91 @@ const CalcConsumoAC: React.FC<CalcConsumoACProps> = ({
   };
 
   return (
-    <div className="border p-4 rounded-md shadow-md">
-      <h2 className="text-lg font-semibold mb-2">
-        Calcular Consumo de Ar (AC)
-      </h2>
-      <div className="mb-4">
-        <label>
-          Consumo de Ar na Superfície (CAS - litros/min):
-          <input
-            type="number"
-            value={cas}
-            onChange={(e) => setCas(parseFloat(e.target.value))}
-            className="border rounded-md p-2"
-          />
-        </label>
-      </div>
-      <div className="mb-4">
-        <label>
-          Volume do Cilindro (V - litros):
-          <input
-            type="number"
-            value={v}
-            onChange={(e) => setV(parseFloat(e.target.value))}
-            className="border rounded-md p-2"
-          />
-        </label>
-      </div>
-      <div className="mb-4">
-        <label>
-          Tempo (t - minutos):
-          <input
-            type="number"
-            value={t}
-            onChange={(e) => setT(parseFloat(e.target.value))}
-            className="border rounded-md p-2"
-          />
-        </label>
-      </div>
-      <div className="mb-4">
-        <label>
-          Pressão absoluta na profundidade (P - bar):
-          <input
-            type="number"
-            value={p}
-            onChange={(e) => setP(parseFloat(e.target.value))}
-            className="border rounded-md p-2"
-          />
-        </label>
-      </div>
-      <button
-        onClick={calculateConsumoAC}
-        className="bg-blue-500 text-white px-4 py-2 rounded-md"
-      >
-        Calcular Consumo AC
-      </button>
-      {result !== null ? (
-        <p className="mt-4">Consumo de Ar (AC): {result.toFixed(2)} bar</p>
-      ) : (
-        <p className="mt-4">
-          Informe os valores CAS, V, t e P para calcular o Consumo de Ar (AC).
-        </p>
-      )}
+<div className="border p-4 rounded-md shadow-md">
+  <h2 className="text-lg font-semibold mb-2">
+    Calcular Consumo de Ar (AC)
+  </h2>
+  <div className='w-full max-w-lg'>
+  <div className="md:flex md:items-center text-right my-4">
+    <div className="md:w-2/3 mr-2">
+      Consumo de Ar na Superfície (CAS - litros/min):
     </div>
+    <div>
+      <input
+        type="number"
+        value={cas}
+        onChange={(e) => setCas(parseFloat(e.target.value))}
+        className={`border rounded-md p-2 ${
+          cas > 0 ? "border-green-500" : "border-red-500"
+        }`}
+      />
+    </div>
+  </div>
+  <div className="md:flex md:items-center text-right my-4">
+    <div className="md:w-2/3 mr-2">
+      Volume do Cilindro (V - litros):
+    </div>
+    <div>
+      <input
+        type="number"
+        value={v}
+        onChange={(e) => setV(parseFloat(e.target.value))}
+        className={`border rounded-md p-2 ${
+          v > 0 ? "border-green-500" : "border-red-500"
+        }`}
+      />
+    </div>
+  </div>
+  <div className="md:flex md:items-center text-right my-4">
+    <div className="md:w-2/3 mr-2">
+      Tempo (t - minutos):
+    </div>
+    <div>
+      <input
+        type="number"
+        value={t}
+        onChange={(e) => setT(parseFloat(e.target.value))}
+        className={`border rounded-md p-2 ${
+          t > 0 ? "border-green-500" : "border-red-500"
+        }`}
+      />
+    </div>
+  </div>
+  <div className="md:flex md:items-center text-right my-4">
+    <div className="md:w-2/3 mr-2">
+      Pressão absoluta na profundidade (P - bar):
+    </div>
+    <div>
+      <input
+        type="number"
+        value={p}
+        onChange={(e) => setP(parseFloat(e.target.value))}
+        className={`border rounded-md p-2 ${
+          p > 0 ? "border-green-500" : "border-red-500"
+        }`}
+      />
+    </div>
+  </div>
+  <div className="text-center my-5">
+    <button
+      onClick={calculateConsumoAC}
+      className={`${
+        cas > 0 && v > 0 && t > 0 && p > 0 ? "bg-green-500" : "bg-gray-500"
+      } text-white px-4 py-2 rounded-md`}
+    >
+      Calcular Consumo AC
+    </button>
+    </div>
+  </div>
+  {result !== null ? (
+    <p className="mt-4">Consumo de Ar (AC): {result.toFixed(2)} bar</p>
+  ) : (
+    <p className="mt-4">
+      Informe os valores CAS, V, t e P para calcular o Consumo de Ar (AC).
+    </p>
+  )}
+</div>
+
   );
 };
 
--- src/components/CalcTempo.tsx
+++ src/components/CalcTempo.tsx
@@ -23,62 +23,91 @@ const CalcTempo: React.FC<CalcTempoProps> = ({ onTempoCalculated }) => {
 
   return (
     <div className="border p-4 rounded-md shadow-md">
-      <h2 className="text-lg font-semibold mb-2">Calcular Tempo (t)</h2>
-      <div className="mb-4">
-        <label>
-          Consumo de Ar na Superfície (CAS - litros/min):
-          <input
-            type="number"
-            value={cas}
-            onChange={(e) => setCas(parseFloat(e.target.value))}
-            className="border rounded-md p-2"
-          />
-        </label>
-      </div>
-      <div className="mb-4">
-        <label>
-          Consumo de Ar (AC - bar):
-          <input
-            type="number"
-            value={ac}
-            onChange={(e) => setAc(parseFloat(e.target.value))}
-            className="border rounded-md p-2"
-          />
-        </label>
-      </div>
-      <div className="mb-4">
-        <label>
-          Volume do Cilindro (V - litros):
-          <input
-            type="number"
-            value={v}
-            onChange={(e) => setV(parseFloat(e.target.value))}
-            className="border rounded-md p-2"
-          />
-        </label>
-      </div>
-      <div className="mb-4">
-        <label>
-          Pressão absoluta na profundidade (P - bar):
-          <input
-            type="number"
-            value={p}
-            onChange={(e) => setP(parseFloat(e.target.value))}
-            className="border rounded-md p-2"
-          />
-        </label>
-      </div>
-      <button onClick={calculateTempo} className="bg-blue-500 text-white px-4 py-2 rounded-md">
-        Calcular Tempo
-      </button>
-      {result !== null ? (
-        <p className="mt-4">
-          Tempo (t): {result.toFixed(2)} minutos
-        </p>
-      ) : (
-        <p className="mt-4">Informe os valores CAS, AC, V e P para calcular o Tempo (t).</p>
-      )}
+    <div>
+    <h2 className="text-lg font-semibold mb-2">Calcular Tempo (t)</h2>
     </div>
+    <div className='w-full max-w-lg'>
+  <div className="md:flex md:items-center text-right my-4">
+    <div className="md:w-2/3 mr-2">
+      Consumo de Ar na Superfície (CAS - litros/min):
+    </div>
+    <div>
+      <input
+        type="number"
+        value={cas}
+        onChange={(e) => setCas(parseFloat(e.target.value))}
+        className={`border rounded-md p-2 ${
+          cas > 0 ? "border-green-500" : "border-red-500"
+        }`}
+      />
+    </div>
+  </div>
+  <div className="md:flex md:items-center text-right my-4">
+    <div className="md:w-2/3 mr-2">
+      Consumo de Ar (AC - bar):
+    </div>
+    <div>
+      <input
+        type="number"
+        value={ac}
+        onChange={(e) => setAc(parseFloat(e.target.value))}
+        className={`border rounded-md p-2 ${
+          ac > 0 ? "border-green-500" : "border-red-500"
+        }`}
+      />
+    </div>
+  </div>
+  <div className="md:flex md:items-center text-right my-4">
+    <div className="md:w-2/3 mr-2">
+      Volume do Cilindro (V - litros):
+    </div>
+    <div>
+      <input
+        type="number"
+        value={v}
+        onChange={(e) => setV(parseFloat(e.target.value))}
+        className={`border rounded-md p-2 ${
+          v > 0 ? "border-green-500" : "border-red-500"
+        }`}
+      />
+    </div>
+  </div>
+  <div className="md:flex md:items-center text-right my-4">
+    <div className="md:w-2/3 mr-2">
+      Pressão absoluta na profundidade (P - bar):
+    </div>
+    <div>
+      <input
+        type="number"
+        value={p}
+        onChange={(e) => setP(parseFloat(e.target.value))}
+        className={`border rounded-md p-2 ${
+          p > 0 ? "border-green-500" : "border-red-500"
+        }`}
+      />
+    </div>
+  </div>
+  <div className="text-center my-5">
+    <button
+      onClick={calculateTempo}
+      className={`${
+        cas > 0 && ac > 0 && v > 0 && p > 0 ? "bg-green-500" : "bg-gray-500"
+      } text-white px-4 py-2 rounded-md`}
+    >
+      Calcular Tempo
+    </button></div>
+  </div>
+  {result !== null ? (
+    <p className="mt-4">
+      Tempo (t): {result.toFixed(2)} minutos
+    </p>
+  ) : (
+    <p className="mt-4">
+      Informe os valores CAS, AC, V e P para calcular o Tempo (t).
+    </p>
+  )}
+</div>
+
   );
 };
 
